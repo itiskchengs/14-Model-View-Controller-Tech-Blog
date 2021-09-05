b6975c522058c2931dcb81f53135f3f50a9fba22
Kenneth Cheng <itskcheng@gmail.com>
2021-09-05 16:37:59 -0400

    Updated connection again

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
@@ -13,9 +14,8 @@ if (process.env.JAWSDB_URL) {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
-    },
+    }
   );
 }
 
 module.exports = sequelize;
-
